54de93d103ea05dcefa2dce453390e722498c473
seth spring <seth.c.spring@gmail.com>
2024-04-24 13:54:33 -0400

    user routes created

--- server/app.py
+++ server/app.py
@@ -20,6 +20,35 @@ db.init_app(app)
 def root():
     return '<h1>SubCycle</h1>'
 
+@app.route('/users', methods=['GET', 'POST'])
+def all_users():
+
+    if request.method == 'GET':
+        users_obj = User.query.all()
+
+        users_dict = []
+        for user in users_obj:
+            users_dict.append(user.to_dict())
+
+        return users_dict, 200
+    
+    elif request.method == 'POST':
+        json = request.get_json()
+        is_duplicate = User.query.filter_by(username=json['username']).first()
+        if is_duplicate:
+            return {'Error': f'{is_duplicate} is already registered, please choose another username'}
+        
+        new_user = User(
+            username=json.get('username'), 
+            password=json.get('password'), 
+            email=json.get('email')
+        )
+        
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user.to_dict(), 201
+
+
 @app.route('/user/<string:username>', methods=['GET'])
 def user_profile(username):
     user = User.query.filter_by(username=username).first()
@@ -27,7 +56,17 @@ def user_profile(username):
     if user:
         return user.to_dict(), 200
     else:
-        return {'error': f'{user} not found'}, 404
+        return {'Error': 'Username does not exist'}, 404
+
+@app.route('/user/<int:id>', methods=['GET'])
+def user_by_id(id):
+    user_id = User.query.filter(User.id == id).first()
+
+    if user_id:
+        return user_id.to_dict(), 200
+    else:
+        return {'Error': f'User does not exist'}, 404
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
