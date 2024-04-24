e4147351b5a4393a0b1598cb093468c1534f1c75
seth spring <seth.c.spring@gmail.com>
2024-04-24 18:32:54 -0400

    app.jsx basic working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,31 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import UserContainer from './containers/UserContainer';
-import SubscriptionContainer from './containers/SubscriptionContainer';
-import EscrowContainer from './containers/EscrowContainer';
-import TransactionContainer from './containers/TransactionContainer';
-import ServiceContainer from './containers/ServiceContainer';
-import Navbar from './components/Navbar';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import UserContainer from './containers/UserContainer';
+// import SubscriptionContainer from './containers/SubscriptionContainer';
+// import EscrowContainer from './containers/EscrowContainer';
+// import TransactionContainer from './containers/TransactionContainer';
+// import ServiceContainer from './containers/ServiceContainer';
+// import Navbar from './components/Navbar';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Switch>
+//           <Route path="/" exact component={Home} />
+//           <Route path="/users" component={UserContainer} />
+//           <Route path="/subscriptions" component={SubscriptionContainer} />
+//           <Route path="/escrow" component={EscrowContainer} />
+//           <Route path="/transactions" component={TransactionContainer} />
+//           <Route path="/services" component={ServiceContainer} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// };
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/users" component={UserContainer} />
-          <Route path="/subscriptions" component={SubscriptionContainer} />
-          <Route path="/escrow" component={EscrowContainer} />
-          <Route path="/transactions" component={TransactionContainer} />
-          <Route path="/services" component={ServiceContainer} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
-
-const Home = () => {
   return <h1>Welcome to SubCycle</h1>;
 };
 
