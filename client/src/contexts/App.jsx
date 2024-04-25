f929c70e8ee5cb5a42761504fd44c26a738bebd1
seth spring <seth.c.spring@gmail.com>
2024-04-25 12:30:52 -0400

    restructured folders and created Navbar.jsx

---
+++ client/src/contexts/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import Navbar from '../components/Navbar';
+
+
+
+const App = () => {
+  return(
+    <div>
+      <h1>Welcome to SubCycle</h1>
+      <Navbar />
+    </div>
+  )
+};
+
+export default App;
