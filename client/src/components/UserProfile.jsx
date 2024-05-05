8e7f5d6bf29ab2a00e83cd1f5a979a168d728d1b
seth spring <seth.c.spring@gmail.com>
2024-05-04 22:12:02 -0400

    removed inefficient code

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -7,44 +7,24 @@ import EscrowAccount from "./EscrowAccount";
 function UserProfile() {
   const { user } = useAuth();
   const [userProfile, setUserProfile] = useState([]);
-  const [escrowAccount, setEscrowAccount] = useState(null); // need to figure out where to call escrowAccount
-
-  const getSubscription = async () => {
-    const response = await get_subscriptions();
-    setUserProfile(response.data);
-  };
-
-  const getAccount = async () => {
-    const response = await get_escrow_account();
-    setEscrowAccount(response.data);
-  };
 
   useEffect(() => {
-    getSubscription();
-    getAccount();
+    const getUserProfile = async () => {
+      const subs = await get_subscriptions();
+
+      setUserProfile(subs.data);
+    };
+
+    getUserProfile();
   }, [user]);
 
-    return (
+  return (
     <div className="user-profile">
       {user && <h1>Welcome back, {user.username}!</h1>}
-      {/* {escrowAccount && (
-        <div>
-          <h2>Escrow Account Details</h2>
-          <p>Balance: {escrowAccount.balance}</p>
-        </div>
-      )} */}
       <EscrowAccount />
       <UserServices userProfile={userProfile} />
     </div>
   );
 }
-//   return (
-//     <div className="user-profile">
-//       <h1>Welcome back!</h1> 
-//       {/* need a conditional here so user.username is not Null when user is not logged in */}
-//       <UserServices userProfile={userProfile} />
-//     </div>
-//   );
-// }
 
 export default UserProfile;
