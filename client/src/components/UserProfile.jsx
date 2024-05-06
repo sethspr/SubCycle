d78454e276a9622f23c836015adff501d8a38cdb
seth spring <seth.c.spring@gmail.com>
2024-05-06 17:55:12 -0400

    render show subscription button for user profile

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "./AuthContext";
-import { get_subscriptions, get_escrow_account } from "../services/api.service";
+import { get_subscriptions } from "../services/api.service";
 import UserServices from "./UserServices";
 import EscrowAccount from "./EscrowAccount";
+import SubscriptionList from "./SubscriptionList"
 
 function UserProfile() {
   const { user } = useAuth();
   const [userProfile, setUserProfile] = useState([]);
+  const [showSubscriptionList, setShowSubscriptionList] = useState(false);
 
   useEffect(() => {
     const getUserProfile = async () => {
@@ -21,6 +23,14 @@ function UserProfile() {
   return (
     <div className="user-profile">
       {user && <h1>Welcome back, {user.username}!</h1>}
+      <button onClick={() => setShowSubscriptionList(!showSubscriptionList)}>
+        {" "}
+        {showSubscriptionList
+          ? "Hide Subscriptions"
+          : "Link Subscriptions to Profile"}
+      </button>
+      {showSubscriptionList && <SubscriptionList />}{" "}
+      {/* Render SubscriptionList if showSubscriptionList is true */}
       <EscrowAccount />
       <UserServices userProfile={userProfile} />
     </div>
