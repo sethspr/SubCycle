d9a53163ec957e3f24a5f0e5debe005f67d73fca
seth spring <seth.c.spring@gmail.com>
2024-05-07 16:28:09 -0400

    mui styling

--- client/src/components/SubscriptionList.jsx
+++ client/src/components/SubscriptionList.jsx
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { add_sub_to_profile } from "../services/api.service";
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
+import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useAuth } from "./AuthContext";
 
 function SubscriptionList() {
-  // State to hold and set the list of subscriptions/services
   const { user } = useAuth();
   const [subscriptions, setSubscriptions] = useState([]);
 
-  // Function to fetch subscriptions from the backend app.py
   const fetchSubscriptions = async () => {
     try {
       const response = await fetch("http://127.0.0.1:5555/services");
@@ -21,13 +19,12 @@ function SubscriptionList() {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data = await response.json();
-      setSubscriptions(data); // Set the subscriptions in state
+      setSubscriptions(data);
     } catch (error) {
       console.error("Failed to fetch subscriptions:", error);
     }
   };
 
-  // useEffect to trigger the fetch when the component mounts
   useEffect(() => {
     fetchSubscriptions();
   }, []);
@@ -49,37 +46,36 @@ function SubscriptionList() {
       <h1>Supported Subscriptions</h1>
       <div className="subscription-grid">
         {subscriptions.map((service) => (
-          <>
-            <Box>
-              <Card variant="outlined">
-                <React.Fragment>
-                  <CardContent>
-                    <div className="flex-auto items-center">
-                      <Typography variant="h4">
-                        {service.company_name}
-                      </Typography>
-
-                      <Typography variant="h6" sx={{ margin: "0 1rem" }}>
-                        ${service.amount}/month
-                      </Typography>
-                    </div>
-
-                    <Typography color="text.secondary" sx={{ fontSize: 16 }}>
-                      {service.description}
-                    </Typography>
-                  </CardContent>
-
-                  <CardActions>
-                    {user && "email" in user && (
-                      <Button onClick={() => handleAddSubscription(service.id)}>
-                        Add subscription
-                      </Button>
-                    )}
-                  </CardActions>
-                </React.Fragment>
-              </Card>
-            </Box>
-          </>
+          <Card key={service.id} sx={{ maxWidth: 400, px: 2, pt: 2 }}>
+            <CardMedia
+              component="img"
+              height="auto"
+              // width='60%'
+              image={service.logo}
+              alt="logo"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {service.company_name}
+              </Typography>
+              <Typography variant="h6" color="text.secondary">
+                ${service.amount}/month
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {service.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              {user && "email" in user && (
+                <Button
+                  onClick={() => handleAddSubscription(service.id)}
+                  size="small"
+                >
+                  Add subscription
+                </Button>
+              )}
+            </CardActions>
+          </Card>
         ))}
       </div>
     </>
