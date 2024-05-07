d25b36ebb4421bc568330df06ddaf81ab6ec8b38
seth spring <seth.c.spring@gmail.com>
2024-05-07 16:28:39 -0400

    mui styling

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "./AuthContext";
-import { get_subscriptions } from "../services/api.service";
+import { get_subscriptions, get_escrow_account } from "../services/api.service";
 import UserServices from "./UserServices";
 import EscrowAccount from "./EscrowAccount";
-import SubscriptionList from "./SubscriptionList"
+import SubscriptionList from "./SubscriptionList";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 function UserProfile() {
   const { user } = useAuth();
@@ -21,19 +25,21 @@ function UserProfile() {
   }, [user]);
 
   return (
-    <div className="user-profile">
-      {user && <h1>Welcome back, {user.username}!</h1>}
-      <button onClick={() => setShowSubscriptionList(!showSubscriptionList)}>
-        {" "}
-        {showSubscriptionList
-          ? "Hide Subscriptions"
-          : "Link Subscriptions to Profile"}
-      </button>
-      {showSubscriptionList && <SubscriptionList />}{" "}
-      {/* Render SubscriptionList if showSubscriptionList is true */}
+    <Box sx={{ mx: "auto", maxWidth: 600, p: 2 }}>
+      {user && (
+        <Typography variant="h4">Welcome back, {user.username}!</Typography>
+      )}
+      <Box mt={2} mb={2}>
+        <button onClick={() => setShowSubscriptionList(!showSubscriptionList)}>
+          {showSubscriptionList
+            ? "Hide Subscriptions"
+            : "Link Subscriptions to Profile"}
+        </button>
+      </Box>
+      {showSubscriptionList && <SubscriptionList />}
       <EscrowAccount />
       <UserServices userProfile={userProfile} />
-    </div>
+    </Box>
   );
 }
 
