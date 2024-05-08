50748fcd38b46b22ca8d2de0ce43c812588478e5
seth spring <seth.c.spring@gmail.com>
2024-05-08 01:31:45 -0400

    cards redone and condition render

--- client/src/components/SubscriptionList.jsx
+++ client/src/components/SubscriptionList.jsx
@@ -1,55 +1,126 @@
 import React, { useState, useEffect } from "react";
-import { add_sub_to_profile } from "../services/api.service";
+import {
+  add_sub_to_profile,
+  get_services,
+  get_subscriptions,
+} from "../services/api.service";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import InfoIcon from "@mui/icons-material/Info";
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
+import Tooltip from "@mui/material/Tooltip";
 import { useAuth } from "./AuthContext";
 
 function SubscriptionList() {
   const { user } = useAuth();
   const [subscriptions, setSubscriptions] = useState([]);
+  const [services, setServices] = useState([]);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = (title, description) => {
+    setModalTitle(title);
+    setModalDesc(description);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalDesc, setModalDesc] = useState("");
+
+  const modalStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const fetchServices = async () => {
+    await get_services().then((response) => {
+      setServices(response.data);
+    });
+  };
 
   const fetchSubscriptions = async () => {
-    try {
-      const response = await fetch("http://127.0.0.1:5555/services");
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      setSubscriptions(data);
-    } catch (error) {
-      console.error("Failed to fetch subscriptions:", error);
-    }
+    await get_subscriptions(user.id)
+      .then((response) => {
+        console.dir(response.data);
+        setSubscriptions(response.data);
+      })
+      .catch(() => {
+        console.error("Failed to fetch subscriptions:", error);
+      });
   };
+
+  useEffect(() => {
+    fetchServices();
+  }, []);
 
   useEffect(() => {
     fetchSubscriptions();
   }, []);
 
   const handleAddSubscription = async (serviceId) => {
-    try {
-      await add_sub_to_profile(serviceId);
-      const updatedSubscriptions = await add_sub_to_profile();
-      setSubscriptions(updatedSubscriptions);
-      alert("Subscription added successfully!");
-    } catch (error) {
-      console.error("Failed to add subscription:", error);
-      alert("Failed to add subscription. Please try again later.");
-    }
+    let alert_status = "";
+
+    await add_sub_to_profile(user.id, serviceId)
+      .then(async () => {
+        alert_status = "Subscription added successfully!";
+
+        await get_subscriptions(user.id)
+          .then((response) => {
+            setSubscriptions(response.data);
+          })
+          .catch(() => {
+            alert_status = "Failed to query subscriptions";
+          });
+      })
+      .catch(() => {
+        alert_status = "Failed to add subscription. Please try again later.";
+      })
+      .finally(() => {
+        alert(alert_status);
+      });
+  };
+
+  const isSignedIn = () => {
+    return user && "email" in user;
+  };
+
+  const isSubscribed = (service_id) => {
+    return (
+      subscriptions &&
+      subscriptions.length > 0 &&
+      subscriptions.map((s) => s.service.id).includes(service_id)
+    );
   };
 
   return (
     <>
       <h1>Supported Subscriptions</h1>
       <div className="subscription-grid">
-        {subscriptions.map((service) => (
-          <Card key={service.id} sx={{ maxWidth: 400, px: 2, pt: 2 }}>
+        {services.map((service) => (
+          <Card
+            className="subscription-card"
+            raised
+            key={service.id}
+            sx={{ maxWidth: 400, px: 2, pt: 2 }}
+          >
             <CardMedia
               component="img"
-              height="auto"
+              height="60px"
+              sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
               // width='60%'
               image={service.logo}
               alt="logo"
@@ -61,23 +132,68 @@ function SubscriptionList() {
               <Typography variant="h6" color="text.secondary">
                 ${service.amount}/month
               </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {service.description}
-              </Typography>
             </CardContent>
             <CardActions>
-              {user && "email" in user && (
-                <Button
-                  onClick={() => handleAddSubscription(service.id)}
-                  size="small"
-                >
-                  Add subscription
-                </Button>
+              <div className="flex-auto items-center">
+                <Tooltip title="More info">
+                  <IconButton
+                    color="primary"
+                    onClick={() =>
+                      handleOpen(service.company_name, service.description)
+                    }
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                </Tooltip>
+
+                {isSignedIn() && isSubscribed(service.id) && (
+                 <Tooltip title="View transactions">
+                 <IconButton color="secondary">
+                   <ReceiptLongIcon />
+                 </IconButton>
+               </Tooltip>
+              )}
+
+               
+              </div>
+
+              {isSignedIn() && !isSubscribed(service.id) && (
+                <div className="flex-auto justify-end items-center">
+                  <Button
+                    onClick={() => handleAddSubscription(service.id)}
+                    size="small"
+                  >
+                    Add subscription
+                  </Button>
+                </div>
+              )}
+
+              {isSignedIn() && isSubscribed(service.id) && (
+                <div className="flex-auto justify-end items-center">
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+
+                  <Button size="small" color="error">
+                    Remove
+                  </Button>
+                </div>
               )}
             </CardActions>
           </Card>
         ))}
       </div>
+
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={modalStyle}>
+          <Typography variant="h6" component="h2">
+            {modalTitle}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {modalDesc}
+          </Typography>
+        </Box>
+      </Modal>
     </>
   );
 }
